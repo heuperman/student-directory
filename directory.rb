b5eff57590dcaa8c34183653526eb9ecad8f8e60
kees <cheuperman@gmail.com>
2018-05-29 17:40:40 +0100

    Add method to list each student's favourite hobby in the directory

--- directory.rb
+++ directory.rb
@@ -4,33 +4,34 @@ def input_students
   students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :June}
-    puts "We now have #{students.count} students"
+    puts "And their favourite hobby".center(60)
+    hobby = gets.chomp
+    students << {name: name, cohort: :June, hobby: hobby}
+    puts "We now have #{students.count} students".center(60)
     name = gets.chomp
   end
   students
 end
 
 def print_header
-  puts "The students of Bela Fleck's Banjo Academy with names shorter than 12 characters"
-  puts "---------------"
+  puts "The students of Bela Fleck's Banjo Academy".center(60)
+  puts "---------------".center(60)
 end
 
 def print(students)
   n = 0
   while n < students.length
-    puts "#{n+1}. #{students[n][:name]} (#{students[n][:cohort]} cohort)"
+    puts "#{n+1}. #{students[n][:name]} (#{students[n][:cohort]} cohort)".center(60)
+    puts "Favourite hobby: #{students[n][:hobby]}".center(60)
     n += 1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} excellent students"
+  puts "Overall, we have #{students.count} excellent students".center(60)
 end
 
 students = input_students
 print_header
 print(students)
 print_footer(students)
-
-#4

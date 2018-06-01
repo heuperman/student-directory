04c1c07fc012ec4e7f1cd40b13ce3ce00beaaa68
kees <cheuperman@gmail.com>
2018-06-01 13:11:04 +0100

    Add option to load list of students from csv file

--- directory.rb
+++ directory.rb
@@ -10,6 +10,15 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def interactive_menu
   loop do
     print_menu
@@ -25,6 +34,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -36,6 +47,7 @@ def print_menu
   puts "1. Input student names"
   puts "2. Show list of students"
   puts "3. Save the list to students.csv"
+  puts "4. Load list from students.csv"
   puts "9. Exit"
 end
 

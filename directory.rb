f13553189135892fae4b39633a92ec72b4b2311a
kees <cheuperman@gmail.com>
2018-06-01 12:59:04 +0100

    Add option to save student list in csv format, add .gitignore to prefent commit of student list

--- directory.rb
+++ directory.rb
@@ -1,5 +1,15 @@
 @students = []
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def interactive_menu
   loop do
     print_menu
@@ -13,6 +23,8 @@ def process(selection)
       @students = input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -23,6 +35,7 @@ end
 def print_menu
   puts "1. Input student names"
   puts "2. Show list of students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 

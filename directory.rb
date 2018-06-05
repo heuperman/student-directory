9f45dd3c2d402901a8953b0865cf999e1ca55d5a
kees <cheuperman@gmail.com>
2018-06-05 13:10:22 +0100

    Refactor

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 @students = []
+@cohorts = []
 
 def try_load_students
   filename = ARGV.first
@@ -12,37 +13,10 @@ def try_load_students
   end
 end
 
-def save_students
-  file = File.open("students.csv", "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
 def interactive_menu
   loop do
     print_menu
     process(STDIN.gets.chomp)
-  end
-end
-
-def process(selection)
-  case selection
-    when "1"
-      @students = input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      puts "Invalid input, please try again"
   end
 end
 
@@ -54,15 +28,44 @@ def print_menu
   puts "9. Exit"
 end
 
+def process(selection)
+  case selection
+    when "1" then @students = input_students
+    when "2" then show_students
+    when "3" then save_students
+    when "4" then load_students
+    when "9" then exit
+    else puts "Invalid input, please try again"
+  end
+end
+
+def input_students
+  puts "Please enter the name of your students and their cohort, seprated by a comma"
+  puts "Just hit enter twice when finished"
+  line = STDIN.gets.chomp
+  until line.empty? do
+    add_student_to_list(line)
+    puts "We now have #{@students.count} " +
+      (@students.length > 1 ? "students." : "student.")
+    line = gets.chomp
+  end
+  @students
+end
+
 def show_students
   print_header
   print_students_list
   print_footer
 end
 
-def add_student_to_list(line)
-  name, cohort = line.chomp.split(',')
-  @students << {name: name, cohort: cohort.to_sym}
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def load_students(filename = "students.csv")
@@ -73,46 +76,43 @@ def load_students(filename = "students.csv")
   file.close
 end
 
-def input_students
-  puts "Please enter the name of your students and their cohort, seprated by a comma"
-  puts "Just hit enter twice when finished"
-  line = STDIN.gets.chomp
-  until line.empty? do
-    add_student_to_list(line)
-    # check if there is more than one student and pluralise if so.
-    if @students.length > 1
-      puts "We now have #{@students.count} students".center(60)
-    else
-      puts "We now have #{@students.count} student".center(60)
-    end
-    line = gets.chomp
-  end
-  @students
+def add_student_to_list(line)
+  name, cohort = line.chomp.split(',')
+  @students << {name: name, cohort: cohort.to_sym}
+  @cohorts << cohort if !@cohorts.include? cohort
 end
 
 def print_header
-  puts "The students of Bela Fleck's Banjo Academy".center(60)
-  puts "---------------".center(60)
+  puts "The students of Bela Fleck's Banjo Academy"
+  puts "---------------"
 end
 
 def print_students_list
-  puts "Which cohort would you like to see?".center(60)
-  cohort = STDIN.gets.chomp.to_sym
+  display_cohort_list
+  cohort = @cohorts[gets.chomp.to_i - 1].to_sym
+  print_cohort_students(cohort)
+end
+
+def display_cohort_list
+  puts "Which cohort would you like to see?"
+  @cohorts.each_with_index do |cohort, i|
+    puts "#{i + 1}. #{cohort}"
+  end
+end
+
+def print_cohort_students(cohort)
   n = 0
   @students.each do |student|
     if student[:cohort] == cohort
-      puts "#{n+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(60)
+      puts "#{n + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
       n += 1
     end
   end
 end
 
 def print_footer
-  if @students.count > 1
-    puts "Overall, we have #{@students.count} excellent students".center(60)
-  else
-    puts "Overall, we have #{@students.count} excellent student".center(60)
-  end
+  puts "Overall, we have #{@students.count} excellent " +
+    (@students.count > 1 ? "students." : "student.")
 end
 
 try_load_students
